cd7f42831ad0741a013518d223637247891cb229
PrevitaliGiacomo <giacomoprevitali26@gmail.com>
2022-10-28 13:46:34 +0200

    un giorno funzionerà

--- Server Form/Server/Form1.cs
+++ Server Form/Server/Form1.cs
@@ -15,116 +15,108 @@ using System.IO;
 namespace Server
 {
     public partial class Form1 : Form
-    { 
-
-        Thread t;
-       
-        IPAddress ipAddress;
-        IPEndPoint localEndPoint;
+    {
         Socket listener;
-        byte[] bytes = new Byte[1024];
-        Random r;
-        bool check = false;
-        bool acceso=false;
-        int num;
+        Thread t;
+        bool acceso = false;
+        bool b = true;
         public static string data = null;
         public Form1()
         {
             CheckForIllegalCrossThreadCalls = false;
             InitializeComponent();
         }
-        private void Form1_Load(object sender, EventArgs e)
-        {            
-            Inizializza();
-        }
-        public void Inizializza()
-        {
-            ipAddress = System.Net.IPAddress.Parse("127.0.0.1");
-            localEndPoint = new IPEndPoint(ipAddress, 5000);
-            listener = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
-            r = new Random();
-        }
-
         private void button1_Click(object sender, EventArgs e)
         {
-            t = new Thread(server);
-            t.IsBackground = true;
-            t.Start();
-
-        }
-        public void server()
-        {
-            Socket handler = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
             if (!acceso)
             {
                 lbl_Stato.Text = "Acceso";
                 lbl_Stato.ForeColor = System.Drawing.Color.Green;
-                acceso = !acceso;
-                try
+                acceso = true;
+                //Application.DoEvents();
+                t = new Thread(server);
+                t.IsBackground = true;
+                t.Start();
+            }
+            else
+            {
+                lbl_Stato.Text = "Spento";
+                lbl_Stato.ForeColor = System.Drawing.Color.Red;
+                acceso = false;
+
+            }
+        }
+        public void server()
+        {
+            bool check = false;
+            Random r;
+            int num;
+            byte[] bytes = new Byte[1024];
+            IPAddress ipAddress = System.Net.IPAddress.Parse("127.0.0.1");
+            IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 5000);
+            listener = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            r = new Random();
+
+            try
                 {
- 
+               // if (b==true)
+                //{
                     listener.Bind(localEndPoint);
                     listener.Listen(10);
+                    b = false;
+                //}
+                while (acceso)
+                {
+                    Socket handler = listener.Accept();
+                    data = null;
 
                     while (true)
                     {
-                        handler = listener.Accept();
-                        data = null;
-
-                        while (true)
+                        int bytesRec = handler.Receive(bytes);
+                        data += Encoding.ASCII.GetString(bytes, 0, bytesRec);
+                        if (data.IndexOf("<EOF>") > -1)
                         {
-                            int bytesRec = handler.Receive(bytes);
-                            data += Encoding.ASCII.GetString(bytes, 0, bytesRec);
-                            if (data.IndexOf("<EOF>") > -1)
-                            {
-                                break;
-                            }
+                            break;
                         }
 
-                        string[] info = data.Split(';');
-                        var reader = new StreamReader(@"Utenti.csv");
-                        while (!reader.EndOfStream)
-                        {
-                            var l = reader.ReadLine();
-                            string[] v = l.Split(';');
-
-                            if (v[0] == info[0] && v[1] == info[1])
-                            {
-                                check = true;
-                            }
-                        }
-                        if (check)
-                        {
-                            num = r.Next(Convert.ToInt32(info[2]), Convert.ToInt32(info[3]));
-                        }
-                        else
-                        {
-                            num = -1;
-                        }
-
-                        byte[] msg = Encoding.ASCII.GetBytes(Convert.ToString(num));
-
-                        handler.Send(msg);
-                        handler.Shutdown(SocketShutdown.Both);
-                        handler.Close();
-                        check = false;
                     }
+
+                    string[] info = data.Split(';');
+                    var reader = new StreamReader(@"Utenti.csv");
+                    while (!reader.EndOfStream)
+                    {
+                        var l = reader.ReadLine();
+                        string[] v = l.Split(';');
+
+                        if (v[0] == info[0] && v[1] == info[1])
+                        {
+                            check = true;
+                        }
+                    }
+                    if (check)
+                    {
+                        num = r.Next(Convert.ToInt32(info[2]), Convert.ToInt32(info[3]));
+                    }
+                    else
+                    {
+                        num = -1;
+                    }
+
+                    byte[] msg = Encoding.ASCII.GetBytes(Convert.ToString(num));
+                    handler.Send(msg);
+                    handler.Shutdown(SocketShutdown.Both);
+                    handler.Close();
+                    check = false;
                 }
+                
+                
+            }
 
                 catch (Exception ex)
                 {
                     MessageBox.Show(ex.ToString());
                    
                 }
-            }
-            else
-            {
-                lbl_Stato.Text = "Spento";
-                lbl_Stato.ForeColor = System.Drawing.Color.Red;
-                acceso = !acceso;
-                
-            }
-
         }
 
         private void button2_Click(object sender, EventArgs e)
